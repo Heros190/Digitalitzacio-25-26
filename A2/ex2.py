5d9af7d65bb87119eb9ec38c68410ea74f3f02df
Heros190 <llorens.abad@gmail.com>
2025-11-23 13:25:42 +0100

    modificar ex5

--- A2/ex2.py
+++ A2/ex2.py
@@ -2,7 +2,8 @@ list_num = [2, 7, 4, 5, 1, 8, 3, 9, 6, 10]
 
 numR = int(input("Introdueix un número per buscar a la llista: "))
 contador = 0
-
+for num in list_num:
+    print(num)
 for num in list_num:
     if num == numR:
         contador += 1
--- A2/ex5.py
+++ A2/ex5.py
@@ -3,8 +3,13 @@ list_num = [2, 7, 4, 2, 1, 6, 3, 7, 6, 10]
 llista_sense_dups = []
 
 for num in list_num:
-    if num not in llista_sense_dups:
-        llista_sense_dups.append(num)
+    if num in llista_sense_dups:
+        llista_sense_dups.remove(num)
+    llista_sense_dups.append(num)
+    
+#for num in list_num:
+#   if num not in llista_sense_dups:
+#        llista_sense_dups.append(num)
 
 print("Llista original:", list_num)
 print("Llista sense duplicats:", llista_sense_dups)
